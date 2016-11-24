448ca5dfdf809489111c38c6130f74d010a952f8
Nguyen Thinh <nguyenthinh@ThinhND.local>
2016-11-24 17:07:50 +0700

    init  

---
+++ node_modules/symple-client/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: [
+                    'src/symple.js',
+                    'src/symple.client.js'
+                ],
+                dest: 'dist/symple.js'
+            }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'dist/symple.min.js': ['dist/symple.js']
+                }
+            }
+        },
+        watch: {
+          files: ['*'],
+          tasks: ['concat', 'uglify']
+       }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['concat:js', 'uglify:js']);
+};
