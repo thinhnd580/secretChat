aa63ee749c71a67f8a1169f8705ea33ae379f57e
thjnh195 <thjnh195@gmail.com>
2016-12-06 00:12:35 +0700

    fuck u

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,8 @@
 // AngilarJS controller
 
 function SympleChat($scope) {
+    $scope.email = $('#emailInput');
+    $scope.password = $('#passwordInput');
     $scope.client;
     $scope.localPlayer;
     $scope.remotePlayer;
@@ -14,19 +16,27 @@ function SympleChat($scope) {
     $scope.testArray = [];
     $scope.messageText = "";
     $scope.errorText = "";
-    $scope.isLoading = false;
+    $scope.isLogined= true;
     $scope.disableLocalAudio = false; // set true to prevent local feedback loop
-    $scope.camera = $('#camera');
+    $scope.camera = $('#ss');
+    $scope.cameraVideo = $('#camera');
+    $scope.cameraHeight = { 'min-height': 0.0 };
+    $scope.hideCamera = true;
+    $scope.loginView = $('loginView');
+    currentUserIndex = -1
 
     $(document).ready(function() {
         
+        // $scope.loginView.modal('show');
         //
         // Client
         $scope.client = new Symple.Client(CLIENT_OPTIONS);
         //Init
-        $scope.handle= "It's Me";
+        $scope.handle= Math.random().toString(36).substr(2, 5);
+        // showCamera(false);
+        // $scope.camera.modal('show');
         $scope.login();
-        $scope.camera.modal('hide');
+
 
         $scope.client.on('announce', function(peer) {
             //console.log('announce:', peer)
@@ -43,21 +53,50 @@ function SympleChat($scope) {
         $scope.client.on('message', function(m) {
             console.log('message:', m)
 
-            // Normal Message
-            if (!m.direct || m.direct == $scope.handle) {
-                // $scope.messages['m.from.user'].push
-                $scope.messages.push({
+            tempMessage = {
                     user: m.from.user,
                     data: m.data,
                     to: m.to,
                     direct: m.direct,
                     time: Symple.formatTime(new Date)
-                });
-                $scope.$apply();
+            };
+            for (var i = 0; i < $scope.testArray.length; i++) {
+                if ($scope.testArray[i].user == m.from.user) {
+                    $scope.testArray[i].messages.push(tempMessage);
+                    break;
+                }
             }
-            else {
-              console.log('dropping message:', m, m.direct)
-            }
+            if (m.from.user == $scope.directUser) {
+                $scope.currentMessages.push(tempMessage);
+            }   
+            $scope.$apply();
+
+            // Normal Message
+            // if (!m.direct || m.direct == $scope.handle) {
+            //     // $scope.messages['m.from.user'].push
+            //     // $scope.messages.push({
+            //     //     user: m.from.user,
+            //     //     data: m.data,
+            //     //     to: m.to,
+            //     //     direct: m.direct,
+            //     //     time: Symple.formatTime(new Date)
+            //     // });
+
+            //     tempMessage = {
+            //         user: m.from.user,
+            //         data: m.data,
+            //         to: m.to,
+            //         direct: m.direct,
+            //         time: Symple.formatTime(new Date)
+            //     };
+            //     $scope.testArray[currentUserIndex].messages.push(tempMessage);
+            //     $scope.currentMessages.push(tempMessage);
+
+            //     $scope.$apply();
+            // }
+            // else {
+            //   console.log('dropping message:', m, m.direct)
+            // }
         });
 
         $scope.client.on('command', function(c) {
@@ -67,6 +106,7 @@ function SympleChat($scope) {
 
                 if (!c.status) {
                     // Show a dialog to the user asking if they want to accept the call
+                    // $scope.hideCamera = false;
                     var e = $('#incoming-call-modal')
                     e.find('.caller').text('@' + c.from.user)
                     e.find('.accept').unbind('click').click(function() {
@@ -75,13 +115,16 @@ function SympleChat($scope) {
                         $scope.client.respond(c);
                         $scope.$apply();
                         e.modal('hide');
+                        $scope.hideCamera = false;
                     })
                     e.find('.reject').unbind('click').click(function() {
                         c.status = 500;
                         $scope.client.respond(c);
                         e.modal('hide');
+                        $scope.hideCamera = true;
                     })
-                    e.modal('show')
+                    e.modal('show');
+                    $scope.hideCamera = false;
                 }
                 else if (c.status == 200) {
                     // Handle call accepted
@@ -191,11 +234,27 @@ function SympleChat($scope) {
 
     $scope.setMessageTarget = function(user) {
         console.log('setMessageTarget', user)
-        $scope.directUser = user ? user : ''
-        $('#post-message .direct-user').text('@' + $scope.directUser)
+        
+        $('#post-message .direct-user').text('@' + user)
         $('#post-message .message-text')[0].focus()
+        if (user != $scope.directUser) {
+            $scope.currentMessages.splice(0,$scope.currentMessages.length);
+            for (var i = 0; i < $scope.testArray.length; i++) {
+                console.log($scope.testArray);
+                if ( $scope.testArray[i].user == user) {
+                    console.log("hehfehfhehf");
+                    for (var j=0; j< $scope.testArray[i].messages.length;j++) {
+                        console.log("fuck u");
+                        console.log($scope.testArray[i].messages[j]);
+                        $scope.currentMessages.push($scope.testArray[i].messages[j]);
+                    }
+                    currentUserIndex = i;
+                    break;
+                }
+            }
+        }
+        $scope.directUser = user ? user : ''
 
-        $scope.currentMessages.splice(0,$scope.currentMessages.length);
     }
 
     $scope.sendMessage = function() {
@@ -229,10 +288,13 @@ function SympleChat($scope) {
             time: Symple.formatTime(new Date)
         });
         $scope.messageText = "";
+        var box = $('#messagesList');
+        box.scrollTop = box.scrollHeight;
     };
 
     // Login
     $scope.login = function() {
+        console.log('FUCKCKCKCKCKCKCKCKCKCK');
         if (!$scope.handle || $scope.handle.length < 3) {
             alert('Please enter 3 or more alphanumeric characters.');
             return;
@@ -244,6 +306,7 @@ function SympleChat($scope) {
         console.log('directUser:', $scope.client.options.peer.user)
         $scope.client.connect();
         $scope.isLoading = true;
+        $scope.isLogined= true;
         //$scope.$apply();
     }
 
@@ -254,6 +317,7 @@ function SympleChat($scope) {
     $scope.startVideoCall = function(user) {
         if (assertGetUserMedia()) {
             console.log('startVideoCall', user)
+            $scope.hideCamera = false;
             if (user == $scope.handle) {
                 alert('Cannot video chat with yourself. Please open a new browser window and login with a different handle.');
                 return;
@@ -268,8 +332,7 @@ function SympleChat($scope) {
 
     $scope.startLocalVideo = function() {
         if (assertGetUserMedia()) {
-
-            // Init local video player
+            // Init local video localPlayer
             $scope.localPlayer = createPlayer($scope, 'caller', '#video .local-video');
             $scope.localPlayer.play({ localMedia: true, disableAudio: $scope.disableLocalAudio });
 
@@ -278,6 +341,10 @@ function SympleChat($scope) {
         }
     }
 
+    $scope.endCallVideo = function() {
+        $scope.hideCamera = true;
+        $scope.localVideoPlaying = false;
+    }
 
     //
     // Helpers
@@ -298,5 +365,13 @@ function SympleChat($scope) {
               arr.splice(i, 1);
           }
       }
-  }
+    }
+
+    function showCamera(showed) {
+        if (showed) {
+            angular.element(document.querySelector('#camera'))[0].style.height = "300px";
+        } else {
+            angular.element(document.querySelector('#camera'))[0].style.height = "0px";
+        }
+    }
 }
